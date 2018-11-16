feec6288fd6ad24d703a125865529d08c9f30fe1
mwolters90 <mark@really-simple-ssl.com>
2018-11-16 11:50:40 +0100

    all popups centered

--- templates/facebook.php
+++ templates/facebook.php
@@ -1,7 +1,7 @@
 <?php defined('ABSPATH') or die("you do not have access to this page!");  ?>
 <a class="post-share facebook"
     href="#"
-    onClick = "rssslPopupCenter('https://www.facebook.com/share.php?u={url}&width&layout=standard&action=like&show_faces=true&share=true&height=80','FB','900','500'); return false;">
+    onClick = "rssslPopupCenter('https://www.facebook.com/share.php?u={url}&width&layout=standard&action=like&show_faces=true&share=true&height=80','Facebook','900','500'); return false;">
     <div class="rsssl_count"><i class="icon-facebook"></i><span class="rsssl_likes_shares">{shares}</span>
     </div>
 </a>
--- templates/google.php
+++ templates/google.php
@@ -1,5 +1,6 @@
 <?php defined('ABSPATH') or die("you do not have acces to this page!"); ?>
-<a class="post-share gplus" href="https://plus.google.com/share?url={url}"
-   onclick="javascript:window.open(this.href, '', 'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=600,width=600');return false;">
+<a class="post-share gplus"
+   href="#"
+   onClick = "rssslPopupCenter('https://plus.google.com/share?url={url}','Google','900','500'); return false;">
     <div class="rsssl_count"><i class="icon-google"></i><span class="rsssl_likes_shares">{shares}<span></div>
 </a>
