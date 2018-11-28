4350a79e640d48d5e95cacc7e0aa2dd0a2f3b4d7
mwolters90 <mark@really-simple-ssl.com>
2018-11-28 10:59:27 +0100

    No notices on Gutenberg edit screen

--- class-licensing.php
+++ class-licensing.php
@@ -36,6 +36,10 @@ static function this() {
 }
 
 public function show_notice_license(){
+//prevent showing the review on edit screen, as gutenberg removes the class which makes it editable.
+$screen = get_current_screen();
+if ( $screen->parent_base === 'edit' ) return;
+
 add_action('admin_print_footer_scripts', array($this, 'dismiss_license_notice_script'));
  $dismissed	= get_option('rsssl_soc_license_notice_dismissed');
  if (!$this->license_is_valid() && !$dismissed) { ?>
@@ -89,6 +93,10 @@ public function dismiss_license_notice_script() {
 
 
 public function show_multisite_notice_license(){
+//prevent showing the review on edit screen, as gutenberg removes the class which makes it editable.
+$screen = get_current_screen();
+if ( $screen->parent_base === 'edit' ) return;
+
 	if (is_multisite()) { ?>
  	<?php if (is_main_site(get_current_blog_id()) && !$this->license_is_valid()) { ?>
 
