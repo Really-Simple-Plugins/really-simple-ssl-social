d028e24bc95fbd233d0e71020f0f7191a2dec831
mwolters90 <mark@really-simple-ssl.com>
2019-01-28 10:29:31 +0100

    gb check

--- functions.php
+++ functions.php
@@ -4,11 +4,9 @@ if (!function_exists('rsssl_uses_gutenberg')) {
     function rsssl_uses_gutenberg()
     {
 
-    if (function_exists('has_block') && !class_exists('Classic_Editor')) {
-    return true;
-    }
-    return false;
+        if (function_exists('has_block') && !class_exists('Classic_Editor')) {
+            return true;
+        }
+        return false;
     }
 }
-
-?>
