f06d9daf0bc76a05f6cfefd0fc3a2d68d9f8200a
mwolters90 <mark@really-simple-ssl.com>
2019-05-03 10:07:44 +0200

    native FB like fix, no ace constant

--- templates/native-facebook.php
+++ templates/native-facebook.php
@@ -1,5 +1,12 @@
-<?php defined('ABSPATH') or die("you do not have access to this page!");  ?>
+<?php defined('ABSPATH') or die("you do not have access to this page!");
+if (get_option('rsssl_fb_button_type') == 'shares') { ?>
 <div class="rsssl-soc-native-item">
-    <div class="fb-share-button" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"></a></div>
+    <div class="fb-share-button" data-href="{url}" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" class="fb-xfbml-parse-ignore"></a></div>
     <div class="rsssl-soc-share-count">{shares}</div>
 </div>
+<?php } else { ?>
+<div class="rsssl-soc-native-item">
+    <div class="fb-like" data-href="{url}" data-width="" data-layout="button" data-action="like" data-size="small" data-show-faces="false" data-share="false"></div>
+    <div class="rsssl-soc-share-count">{shares}</div>
+</div>
+<?php } ?>
