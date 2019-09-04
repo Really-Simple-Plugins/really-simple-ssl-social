24bf0cabe170acc0f7896e358ce79ce5e69152b9
Rogier Lankhorst <rogier@really-simple-plugins.com>
2019-09-04 14:00:43 +0200

    version bug

--- functions.php
+++ functions.php
@@ -10,3 +10,24 @@ if (!function_exists('rsssl_uses_gutenberg')) {
         return false;
     }
 }
+
+
+
+////Lets add Open Graph Meta Info
+//function rsssl_insert_fb_in_head() {
+//    global $rsssl_soc_social;
+//    $url = $rsssl_soc_social->use_http() ? str_replace("https://","http://",get_permalink()) : get_permalink();
+//    echo '<meta property="og:url" content="' . $url . '"/>
+//    ';
+//
+//}
+//add_action( 'wp_head', 'rsssl_insert_fb_in_head', 5 );
+//
+///
+
+
+//function rsssl_insert_upgrade_insecure_requests_header() {
+//    echo '<meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">';
+//
+//}
+//add_action( 'wp_head', 'rsssl_insert_upgrade_insecure_requests_header', 5 );
