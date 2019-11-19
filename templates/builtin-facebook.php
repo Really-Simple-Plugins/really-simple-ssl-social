e6b62ff9deaf44e68be89d44a47ea1531b27013c
mwolters90 <mark@really-simple-ssl.com>
2019-11-19 12:36:33 +0100

    template

--- templates/builtin-facebook.php
+++ templates/builtin-facebook.php
@@ -2,7 +2,7 @@
 if (get_option('rsssl_fb_button_type') == 'shares') { ?>
 <a class="post-share facebook"
     href="#"
-    onClick = "rssslPopupCenter('https://www.facebook.com/share.php?u={url}&width&layout=standard&action={like_or_share}&show_faces=true&share=true&height=80','Facebook','{width}','{height}'); return false;">
+    onClick = "rssslPopupCenter('https://www.facebook.com/share.php?u={url}&width&layout=standard&action=share&show_faces=true&share=true&height=80','Facebook','{width}','{height}'); return false;">
     <div class="rsssl_count">
         {color_round}
         <i class="icon-facebook"></i>{label}<span class="rsssl_likes_shares">{shares}</span>
@@ -10,3 +10,13 @@ if (get_option('rsssl_fb_button_type') == 'shares') { ?>
 </a>
 <?php } else { ?>
 <a class="post-share facebook"
+   href="#"
+   onClick = "rssslPopupCenter('https://www.facebook.com/plugins/like.php?href={url}&layout=standard&action=like&size=large&show_faces=true&share=false&height=65','Facebook','{width}','{height}'); return false;">
+	<div class="rsssl_count">
+		{color_round}
+		<i class="icon-facebook"></i>Like<span class="rsssl_likes_shares">{shares}</span>
+	</div>
+</a>
+<?php }
+
+
